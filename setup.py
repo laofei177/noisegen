b2a098af09ea75b0ec3ff45f6e9970ccf3256137
Paul Brookes <paul.s.brookes@gmail.com>
2020-02-14 17:42:58 +0000

    Update setup file with required packages.

--- setup.py
+++ setup.py
@@ -4,4 +4,10 @@ from setuptools import setup, find_packages
 setup(name='noisegen',
       version='1.0.0',
       description='A package to generate Gaussian distributed noise according to a user specified power spectral density.',
-      packages=find_packages())
+      packages=find_packages(),
+      install_requires=[
+            'matplotlib',
+            'numpy',
+            'pandas',
+            'tqdm']
+      )
